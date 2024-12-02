172da4ace81d10cb0b11b4171509da37667b16a5
Ethan (Sae-Jin) Moon <108815730+saejin-moon@users.noreply.github.com>
2024-12-02 16:10:47 -0500

    made the rust files for dec 2 more rust-y and not just a port from node. lots of vector functions for me to look into.

--- dec2/one.rs
+++ dec2/one.rs
@@ -7,24 +7,13 @@ fn main() {
     for line in file.trim().lines() {
         let nums: Vec<i32> = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
         let increasing: bool = nums[0] < nums[1];
-        let mut safe: bool = true;
-        for i in 0..nums.len() - 1 {
+        let safe: bool = (0..nums.len() - 1).all(|i| {
             let a: i32 = nums[i];
             let b: i32 = nums[i + 1];
-            let diff = (a - b).abs();
-            if diff < 1 || diff > 3 {
-                safe = false;
-                break;
-            }
-            else if increasing && a > b {
-                safe = false;
-                break;
-            } 
-            else if !increasing && a < b {
-                safe = false;
-                break;
-            }
-        }
+            let diff: i32 = (a - b).abs(); // absolute value of the difference
+            // diff between 1 & 3, and increasing or decreasing following the pattern of the rest of the vector
+            (diff >= 1 && diff <= 3) && !(increasing && a > b) && !(!increasing && a < b)
+        });
         if safe {
             count += 1;
         }
--- dec2/two.rs
+++ dec2/two.rs
@@ -1,29 +1,15 @@
 use std::fs;
 
 fn order(nums: &[i32]) -> bool {
-    let mut count = 0;
-    for i in 0..nums.len() - 1 {
-        if nums[i] > nums[i + 1] {
-            count -= 1;
-        } else if nums[i] < nums[i + 1] {
-            count += 1;
-        }
-    }
-    return count > 0
+    nums.windows(2).map(|w| w[1].cmp(&w[0]) as i32).sum::<i32>() > 0
 }
 
-fn is_safe(nums: &[i32]) -> bool {
-    let mut copy = nums.to_vec();
+fn is_safe(nums: &Vec<i32>) -> bool {
+    let mut copy = nums.clone();
     if order(nums) {
         copy.reverse();
     }
-    for i in 0..copy.len() - 1 {
-        let diff = copy[i] - copy[i + 1];
-        if diff < 1 || diff > 3 {
-            return false;
-        }
-    }
-    return true
+    copy.windows(2).all(|w| (1..=3).contains(&(w[0] - w[1])))
 }
 
 fn main() {
@@ -33,17 +19,13 @@ fn main() {
     for line in file.trim().lines() {
         let nums: Vec<i32> = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
         if !is_safe(&nums) {
-            let mut counted = false;
             for i in 0..nums.len() {
                 let mut copy = nums.clone();
                 copy.remove(i);
                 if is_safe(&copy) {
-                    counted = true;
+                    count += 1;
                     break;
                 }
-            }
-            if counted {
-                count += 1;
             }
         } else {
             count += 1;
