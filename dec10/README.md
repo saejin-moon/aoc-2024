5b49e275c9c9bcce42e7b17291121850397b43e0
Ethan (Sae-Jin) Moon <108815730+saejin-moon@users.noreply.github.com>
2024-12-10 19:32:41 -0500

    dec 10, node

---
+++ dec10/README.md
@@ -0,0 +1,4 @@
+**i might give up on rust 'till later or after december to convert these.** <br>
+written in node. <br>
+written in under thirty minutes. <br>
+two stars.
---
+++ dec10/one.mjs
@@ -0,0 +1,33 @@
+import fs from "fs/promises";
+
+(async () => {
+    const file = (await fs.readFile("input.txt", "utf8")).trim();
+    const directions = [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1]
+    ]
+    const data = file.split('\n').map(row => row.split('').map(el => el === "." ? -1 : Number(el)))
+    let zeroes = [];
+    data.forEach((row, i) => row.forEach((cell, j) => { if(cell === 0) return zeroes.push({ x: j, y: i, val: 0 }) }))
+    const nines = new Set();
+    zeroes.forEach((zero, i) => {
+        const queue = [zero];
+        while(queue.length > 0){
+            const { x, y, val } = queue.shift();
+            if(val === 9){
+                nines.add(`${x},${y},${i}`);
+                continue;
+            }
+            directions.forEach(([dx, dy]) => {
+                const nx = x + dx;
+                const ny = y + dy;
+                if(ny >= 0 && ny < data.length && nx >= 0 && ny < data[0].length && data[ny]?.[nx] === val + 1){
+                    queue.push({ x: nx, y: ny, val: val + 1 })
+                }
+            })
+        }
+    })
+    console.log(nines.size);
+})()
